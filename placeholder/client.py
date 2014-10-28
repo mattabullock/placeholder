567d6c480ea7f6f930b91c16cf6d872993527433
Matt Bullock <salty_cea_dog@hotmail.com>
2014-10-27 23:14:36 -0500

    i sent raj jain over the internet

--- placeholder/client.py
+++ placeholder/client.py
@@ -1,10 +1,19 @@
 import socket
 import signal
 import sys
+import util
+
 def signal_handler(signal, frame):
         print('You pressed Ctrl+C!')
+        global s
+        s.close()
+        global f
+        f.close()
         sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
+
+f = open('/Users/matt/Desktop/jain8cr.jpg', 'rb')
+fread = f.read()
 
 TCP_IP = 'localhost'
 TCP_PORT = 5715
@@ -13,17 +22,24 @@ BUFFER_SIZE = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 while 1:
-    data = s.recv(BUFFER_SIZE).rstrip()
-    if not data: break
+    size = util.getSizeOfCommand(s)
+    print "size: " + str(size)
+    data = s.recv(1024).rstrip()
+    print "data: " + data
+    if not data:
+        break
     if data == "100":
         output = "hello"
     elif data == "143":
-        output = "screenshot"
+        output = "!" + str(len(fread))
+        util.sendMessage(s,output)
+        output = "!" + fread
+        util.sendMessage(s,output)
+        print "done sending"
     elif data == "144":
         output = "other thing"
     elif data == "145":
         output = "last thing"
     else:
         output = "i don't know that command"
-    s.send(output+"\n")  
 s.close()
--- placeholder/testserver.py
+++ placeholder/testserver.py
@@ -1,15 +1,34 @@
-import socket 
+import socket
+import util
+import sys
+import signal
+
+def signal_handler(signal, frame):
+        print('You pressed Ctrl+C!')
+        global s
+        s.close()
+        sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+f = open('/Users/matt/Desktop/pic.jpg','w')
 
 host = '' 
-port = 50000 
-backlog = 5 
-size = 1024 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.bind((host,port)) 
-s.listen(backlog) 
-while 1: 
-    client, address = s.accept() 
-    data = client.recv(size) 
-    if data: 
-        client.send(data) 
+port = 5715 
+backlog = 5
+size = 1
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host,port))
+s.listen(backlog)
+while 1:
+    client, address = s.accept()
+    inp = raw_input("Enter command: ")
+    inp = "!" + str(len(inp)) + "!" + inp
+    print inp
+    client.send(inp)
+    for i in range(0,1):
+        size = util.getSizeOfCommand(client)
+        data = client.recv(size)
+        f.write(data)
+        print data 
+
     client.close()
