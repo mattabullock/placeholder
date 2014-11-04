bb97e540ba13cd861e71ba47540ff88f05353eb0
Robert <millerrt3@gmail.com>
2014-11-03 20:25:32 -0600

    Fixed an issue with spaces in path names causing explosions

--- placeholderInstaller/src/main/DownloaderMain.java
+++ placeholderInstaller/src/main/DownloaderMain.java
@@ -38,7 +38,7 @@ public class DownloaderMain
 				String jarPath = DownloaderMain.class.getProtectionDomain().getCodeSource().getLocation().getPath();
 				String decodedPath = URLDecoder.decode(jarPath, "UTF-8");
 				decodedPath = decodedPath.substring(1, decodedPath.length());
-				Elevator.executeAsAdministrator(System.getProperty("java.home") + "\\bin\\java", "-jar " + decodedPath);
+				Elevator.executeAsAdministrator(System.getProperty("java.home") + "\\bin\\java", "-jar \"" + decodedPath + "\"");
 			} catch (UnsupportedEncodingException e) {
 				e.printStackTrace();
 			}
