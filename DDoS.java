5095df67357ef213b21882c3bff5103ca6aee0b4
Robert <millerrt3@gmail.com>
2014-11-06 14:09:39 -0600

    Java DDoS framework for Garrett

---
+++ DDoS.java
@@ -0,0 +1,33 @@
+
+
+public class DDoS
+{
+	String addr;
+	int port;
+
+	public DDoS(String addr, int port)
+	{
+		this.addr = addr;
+		this.port = port;
+	}
+
+	public void initiateAttack()
+	{
+
+	}
+
+	public void endAttack()
+	{
+
+	}
+
+	private int timeoutTest() 
+	{
+		int timeout = 1;
+
+		// loop until connection resets
+
+		return timeout;
+	}
+
+}
